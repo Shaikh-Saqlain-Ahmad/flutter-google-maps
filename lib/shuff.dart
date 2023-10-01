38e45350222e3452e295a6db4379df8f5527b282
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-10-01 23:38:27 +0500

    google markers and button to return to your location

--- lib/shuff.dart
+++ lib/shuff.dart
@@ -12,19 +12,48 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final Completer<GoogleMapController> _controller = Completer();
-  static const CameraPosition _kgooglePlex = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085759655962), zoom: 14.45);
+  static const CameraPosition _kgooglePlex =
+      CameraPosition(target: LatLng(33.6844, 73.0479), zoom: 14.45);
+  List<Marker> _marker = [];
+  final List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId("1"),
+        position: LatLng(33.6844, 73.0479),
+        infoWindow: InfoWindow(title: "My location")),
+    Marker(
+        markerId: MarkerId("2"),
+        position: LatLng(33.0004, 73.5555),
+        infoWindow: InfoWindow(title: "Yahan thula hai"))
+  ];
+
+  void initState() {
+    super.initState();
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kgooglePlex,
-        mapType: MapType.normal, //satellite etc.
-        myLocationEnabled: true,
-        compassEnabled: false,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kgooglePlex,
+          mapType: MapType.normal, //satellite etc.
+          myLocationEnabled: true,
+          compassEnabled: false,
+          markers: Set<Marker>.of(_marker),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(target: LatLng(33.0004, 73.5555), zoom: 14)));
+          setState(() {});
         },
+        child: Icon(Icons.location_disabled_sharp),
       ),
     );
   }
