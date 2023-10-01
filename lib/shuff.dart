a1daab5dc3c06fd8c3104b788623c09d160edc32
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-10-01 14:42:22 +0500

    map working

--- lib/shuff.dart
+++ lib/shuff.dart
@@ -1,15 +1,31 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Shuff extends StatefulWidget {
-  const Shuff({super.key});
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<Shuff> createState() => _ShuffState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _ShuffState extends State<Shuff> {
+class _HomeScreenState extends State<HomeScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+  static const CameraPosition _kgooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085759655962), zoom: 14.45);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _kgooglePlex,
+        mapType: MapType.normal, //satellite etc.
+        myLocationEnabled: true,
+        compassEnabled: false,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
   }
 }
