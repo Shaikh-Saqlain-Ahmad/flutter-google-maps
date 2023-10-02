af05466c651b23aa4753420cf77efbfc111def7d
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-10-02 16:14:39 +0500

    geo locator used to get current location and move the user there

--- lib/shuff.dart
+++ lib/shuff.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,21 +16,25 @@ class _HomeScreenState extends State<HomeScreen> {
   static const CameraPosition _kgooglePlex =
       CameraPosition(target: LatLng(33.6844, 73.0479), zoom: 14.45);
   List<Marker> _marker = [];
-  final List<Marker> _list = const [
+  final List<Marker> _list = [
     Marker(
         markerId: MarkerId("1"),
         position: LatLng(33.6844, 73.0479),
         infoWindow: InfoWindow(title: "My location")),
-    Marker(
-        markerId: MarkerId("2"),
-        position: LatLng(33.0004, 73.5555),
-        infoWindow: InfoWindow(title: "Yahan thula hai"))
   ];
-
-  void initState() {
-    super.initState();
-    _marker.addAll(_list);
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) {
+      print("this error encountered: " + error.toString());
+    });
+    return await Geolocator.getCurrentPosition();
   }
+
+  // void initState() {
+  //   super.initState();
+  //   _marker.addAll(_list);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -48,10 +53,20 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          GoogleMapController controller = await _controller.future;
-          controller.animateCamera(CameraUpdate.newCameraPosition(
-              CameraPosition(target: LatLng(33.0004, 73.5555), zoom: 14)));
-          setState(() {});
+          getUserCurrentLocation().then((value) async {
+            print("my location");
+            print(value.latitude.toString() + " " + value.longitude.toString());
+            _marker.add(Marker(
+                markerId: MarkerId("3"),
+                position: LatLng(value.latitude, value.longitude),
+                infoWindow: InfoWindow(title: "My Location")));
+            CameraPosition cameraPosition = CameraPosition(
+                target: LatLng(value.latitude, value.longitude), zoom: 14);
+            final GoogleMapController controller = await _controller.future;
+            controller
+                .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+            setState(() {});
+          });
         },
         child: Icon(Icons.location_disabled_sharp),
       ),
